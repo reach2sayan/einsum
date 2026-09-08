d19d4ea01bc4419ccf312379cf779e92c420a8c1
Sayan <reach2sayan@gmail.com>
2026-09-07 22:13:51 -0400

    fix CI 3

--- cmake/EinsumVendoredHeaders.cmake
+++ cmake/EinsumVendoredHeaders.cmake
@@ -127,7 +127,8 @@ set(EINSUM_BOOST_HEADER_DIRS
         boost/preprocessor/tuple/limits
         boost/preprocessor/variadic
         boost/preprocessor/variadic/detail
-        boost/preprocessor/variadic/limits)
+        boost/preprocessor/variadic/limits
+        boost/stl_interfaces)
 
 set(EINSUM_EIGEN_HEADER_DIRS
         Eigen
